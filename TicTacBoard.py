d4d270e4dc5e548ea830eb89ef12c1ea13a5e272
gharabin <perryharabin@gmail.com>
2018-03-23 22:23:37 -0400

    Add files via upload

--- TicTacBoard.py
+++ TicTacBoard.py
@@ -4,21 +4,4 @@ Mat = []
 for i in range(size):
     Mat.append(['E']*size)
 
-def printboard(Mat):
-    for i in Mat:
-        print(i)
-    return None
-
-def movex(r,c,Mat):
-    if r>size or c>size:
-        print("Invalid Move")
-    else:
-        Mat[r][c] = "X"
-    return None
-
-def movey(r,c,Mat):
-    if r>size or c>size:
-        print("Invalid Move")
-    else:
-        Mat[r][c] = "Y"
-    return None
+while checkend(Mat) == 0:
